7847d653322ad4a25dcd2f417e196db64a43d5b6
Rabin Ramarajan <suriyarabin@gmail.com>
2023-03-05 13:22:58 +0530

    changes

--- js/contact_custom.js
+++ js/contact_custom.js
@@ -17,6 +17,17 @@ $(document).ready(function()
 {
 	"use strict";
 
+	  // Spinner
+	  var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+
 	/* 
 
 	1. Vars and Inits
